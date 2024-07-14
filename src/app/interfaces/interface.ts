c9ea2e9e8ecfa6e4edffea896c60418b88b33cef
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-13 23:59:05 -0300

    feat: creates the IAttack, IPokeInfo, IPokeTiers interfaces

---
+++ src/app/interfaces/interface.ts
@@ -0,0 +1,27 @@
+export interface IAttack {
+  type: string;
+  fast_attack: string;
+  charged_attack: string;
+}
+
+export interface IPokeInfo {
+  name: string;
+  poke_api: string;
+  types: string[];
+  attacks: IAttack[];
+  is_shiny_available: boolean;
+  is_shadow: boolean;
+  is_mega_or_primal: boolean;
+}
+
+interface IPokeTiers {
+    "S Tier": IPokeInfo[];
+    "A+ Tier": IPokeInfo[];
+    "A Tier": IPokeInfo[];
+    "B+ Tier": IPokeInfo[];
+    "B Tier": IPokeInfo[];
+    "C Tier": IPokeInfo[];
+    "D Tier": IPokeInfo[];
+}
+
+export default IPokeTiers;
