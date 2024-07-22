1c314e3bd2a24d16a237ae4d8f951260b0418076
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-22 13:15:13 -0300

    chore: add CSS and Tailwind CSS configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {extend: {}},
+  theme: {extend: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      accent: 'var(--color-accent)',
+      neutral: 'var(--color-neutral)',
+      background: 'var(--color-background)',
+      text: 'var(--color-text)',
+    }
+  }},
   plugins: [],
 };
 export default config;
