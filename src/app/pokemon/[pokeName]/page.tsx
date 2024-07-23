31b950c6daa0d5d7a98616b7451f83d991f2c18d
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-23 13:15:09 -0300

    feat: create dynamic page to display Pokémon details

---
+++ src/app/pokemon/[pokeName]/page.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import { IPokeTier, IPokeType } from "@/interfaces";
+import { getData } from "@/utils";
+import "./pokeDetails.css";
+
+interface Props {
+  params: { pokeName: string };
+}
+
+function removeUnderscore(str: string) {
+  const decoded = decodeURIComponent(str);
+  return decoded.replace(/_/g, " ");
+}
+
+export default async function PokemonDetails({ params: { pokeName } }: Props) {
+  const [tier_tmp, name_tmp] = pokeName.split("-");
+  const tier = removeUnderscore(tier_tmp);
+  const name = removeUnderscore(name_tmp);
+
+  const allData = await getData<IPokeTier>("pokeTiers.json");
+  const listTypes = await getData<IPokeType>("typesPokemon.json");
+  const pokeInfo = allData[tier].filter((poke) => poke.name === name)[0];
+
+  const imageDescription = `Imagem do Pokémon ${pokeInfo.name}`;
+  let urlImage: string;
+
+  try {
+    const response = await fetch(pokeInfo.poke_api);
+    const data = await response.json();
+    urlImage = data.sprites.other["official-artwork"].front_default;
+  } catch (error) {
+    urlImage = "/whoIsThatPokemon.png";
+  }
+
+  return (
+    <main className="main-poke-details">
+      <h2 className="title-page">Detalhes do Pokémon</h2>
+      <h3 className="name-poke">{pokeInfo.name}</h3>
+      <div className="flex justify-center mb-6">
+        <Image
+          src={urlImage}
+          title={imageDescription}
+          alt={imageDescription}
+          height={128}
+          width={128}
+          className="image-poke"
+        />
+      </div>
+      <h4 className="ranking-position">{tier}</h4>
+      <h4 className="title-types">Tipos:</h4>
+      <div className="types-container">
+        {pokeInfo.types.map((type, index) => (
+          <span key={index} className="type-envelope">
+            {listTypes[type]}
+          </span>
+        ))}
+      </div>
+
+      <h2 className="title-attack">Ataques</h2>
+      {pokeInfo.attacks.length > 0 ? (
+        <div className="overflow-x-auto">
+          <table className="table-attack">
+            <thead className="table-head">
+              <tr>
+                <th className="th-head">Tipo</th>
+                <th className="th-head">Ataque Rápido</th>
+                <th className="th-head">Ataque Carregado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokeInfo.attacks.map((attack, index) => (
+                <tr key={index} className="border-b border-neutral">
+                  <td className="py-2 px-4">{attack.type}</td>
+                  <td className="py-2 px-4">{attack.fast_attack}</td>
+                  <td className="py-2 px-4">{attack.charged_attack}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p className="text-neutral text-center">Nenhum ataque disponível.</p>
+      )}
+    </main>
+  );
+}
