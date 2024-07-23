c0e1fe3c16f9091552c5e8a65c158c6a2e87896a
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-23 15:47:33 -0300

    refactor: redesign and style Header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+import "./header.css";
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,18 +11,18 @@ export function Header() {
   };
 
   return (
-    <header className="bg-primary text-white p-4 md:p-6 shadow-md">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl md:text-3xl font-bold text-accent hover:text-secondary transition-colors">
+    <header className="main-header">
+      <div className="big-screen">
+        <Link href="/" className="title-website">
           Pokemon Go Tier List
         </Link>
-        <nav className="hidden md:flex space-x-4">
-          <Link href="/" className="text-lg text-white hover:text-secondary transition-colors">Home</Link>
-          <Link href="/about" className="text-lg text-white hover:text-secondary transition-colors">About</Link>
-          <Link href="/contact" className="text-lg text-white hover:text-secondary transition-colors">Contact</Link>
+        <nav className="nav-pages">
+          {/* <Link href="/" className="internal-link">Home</Link> */}
+          <Link href="/about" className="internal-link">Sobre</Link>
+          <Link href="/contact" className="internal-link">Contato</Link>
         </nav>
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
+          <button onClick={toggleMenu} className="button-hamburger">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
@@ -29,10 +30,10 @@ export function Header() {
         </div>
       </div>
       {isMenuOpen && (
-        <nav className="md:hidden bg-primary text-white p-4 space-y-2">
-          <Link href="/" className="block text-lg text-white hover:text-secondary transition-colors">Home</Link>
-          <Link href="/about" className="block text-lg text-white hover:text-secondary transition-colors">About</Link>
-          <Link href="/contact" className="block text-lg text-white hover:text-secondary transition-colors">Contact</Link>
+        <nav className="nav-hamburger">
+          {/* <Link href="/" className="block internal-link">Home</Link> */}
+          <Link href="/about" className="block internal-link">Sobre</Link>
+          <Link href="/contact" className="block internal-link">Contato</Link>
         </nav>
       )}
     </header>
