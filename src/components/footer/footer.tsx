4b40441b5ae3324cffd61ed484f9cb5d38354b58
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-23 15:55:24 -0300

    refactor: redesign and style Footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,31 @@
 import Link from "next/link";
+import "./footer.css";
 
 export function Footer() {
   return (
-    <footer style={{ background: "blue" }}>
-      <Link href="/">
-        <h4>Rodapé</h4>
-      </Link>
+    <footer className="main-footer">
+      <div className="main-conteiner">
+        <p className="mb-2">
+          © 2024 Pokemon Go Tier List. Licensed under the{" "}
+          <Link
+            href="https://www.gnu.org/licenses/gpl-3.0.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent underline"
+          >
+            GNU General Public License v3.0
+          </Link>
+          .
+        </p>
+        <nav className="nav-footer">
+          <Link href="/about" className="text-secondary underline">
+            About
+          </Link>
+          <Link href="/contact" className="text-secondary underline">
+            Contact
+          </Link>
+        </nav>
+      </div>
     </footer>
   );
 }
