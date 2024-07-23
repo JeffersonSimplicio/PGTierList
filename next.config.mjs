4f3abaed35101e32f6f399252f07b692ed91453c
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-23 14:23:01 -0300

    feat: add image URL configuration to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,18 @@ const nextConfig = {
             port: '',
             pathname: '/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/**',
           },
+          {
+            protocol: 'https',
+            hostname: 'instagram.fcpv12-1.fna.fbcdn.net',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'media.licdn.com',
+            port: '',
+            pathname: '/dms/image/C4E03AQHSlCjSdDOAlA/**',
+          },
         ],
     },
 };
