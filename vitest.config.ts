a1379e0dbdd8c928882ec76707e7aab6bf814569
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2024-07-26 16:49:33 -0300

    chore: add path alias configuration in Vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
